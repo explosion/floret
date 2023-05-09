6f78c2675a6f6f7224b1c4b231ebcb2abeece100
Adriane Boyd <adrianeboyd@gmail.com>
2023-05-09 11:47:49 +0200

    Add python classifiers for 3.10 and 3.11 (#26)

--- setup.py
+++ setup.py
@@ -171,6 +171,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
         'Topic :: Software Development',
         'Topic :: Scientific/Engineering',
         'Operating System :: Microsoft :: Windows',
